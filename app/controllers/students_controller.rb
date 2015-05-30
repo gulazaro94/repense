d194efc8e78ae631a7b6b1fd0349e592b72afde0
Gustavo <gulazaro94@gmail.com>
2015-05-30 11:36:58 -0300

    user edit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
 
+  before_action :set_student, only: [:edit, :update]
+
   def index
     @students = Student.all
   end
@@ -17,9 +19,25 @@ class StudentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to students_path, notice: t('.success')
+    else
+      render :edit
+    end
+  end
+
   private
 
   def student_params
     params.require(:student).permit(:name, :register_number)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
--- spec/features/students_spec.rb
+++ spec/features/students_spec.rb
@@ -10,14 +10,14 @@ describe StudentsController do
     end
 
     it 'with valid data' do
-      student_name = 'New Student Name'
-      fill_in 'Nome', with: student_name
+      new_name = 'New Student Name'
+      fill_in 'Nome', with: new_name
       fill_in 'Numero de registro', with: '123456'
       click_button 'Criar Estudante'
 
       expect(current_path).to eq(students_path)
       expect(page).to have_content('Estudante criado com sucesso!')
-      expect(Student.last.name).to eq(student_name)
+      expect(Student.last.name).to eq(new_name)
     end
 
     it 'with invalid data' do
@@ -25,6 +25,37 @@ describe StudentsController do
 
       expect(page).to have_content('Nome não pode ficar em branco')
       expect(Student.all).to be_empty
+    end
+  end
+
+  context 'edit' do
+
+    before do
+      @student = create :student
+      visit root_path
+      click_link 'Estudantes'
+      click_link 'Editar'
+    end
+
+    it 'with valid data' do
+      edited_name = 'Edited Student Name'
+      edited_register_number = '654321'
+
+      fill_in 'Nome', with: edited_name
+      fill_in 'Numero de registro', with: edited_register_number
+      click_button 'Atualizar Estudante'
+
+      expect(current_path).to eq(students_path)
+      expect(page).to have_content('Estudante atualizado com sucesso!')
+      expect(@student.reload.name).to eq(edited_name)
+      expect(@student.register_number).to eq(edited_register_number)
+    end
+
+    it 'with invalid data' do
+      fill_in 'Nome', with: ''
+      click_button 'Atualizar Estudante'
+
+      expect(page).to have_content('Nome não pode ficar em branco')
     end
   end
 end
